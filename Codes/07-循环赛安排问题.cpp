78ac20e56e0665ee9132e395a64e63ec5933c71c
Teacher Peng <pengsw@mail.buct.edu.cn>
2019-03-18 14:29:13 +0800

    Create 07-循环赛安排问题.cpp

---
+++ Codes/07-循环赛安排问题.cpp
@@ -0,0 +1,63 @@
+﻿#include <cstdio>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> GetSchedule(int m)
+{
+    // 约定人数n是2的m次幂
+    int n = 1 << m;
+
+    // 分配赛程表的存储空间
+    vector<vector<int>> aSchedule(n);
+    
+    // 开始填表
+    for (int i = 1; i < n; i += i)
+    {
+        // 补充缺少的一列
+        for (int s = 0; s < n; s += i + i)
+        {
+            for (int k = s; k < s + i; k++)
+            {
+                aSchedule[k].push_back(k + i + 1);
+                aSchedule[k + i].push_back(k + 1);
+            }
+        }
+
+        // 复制相邻表
+        for (int j = 0; j < i - 1; j++)
+        {
+            for (int s = 0; s < n; s += i + i)
+            {
+                for (int k = s; k < s + i; k++)
+                {
+                    aSchedule[k].push_back(aSchedule[k + i][j]);
+                    aSchedule[k + i].push_back(aSchedule[k][j]);
+                }
+            }
+        }
+    }
+
+    // 返回结果表
+    return aSchedule;
+}
+
+int PrintSchedule(const vector<vector<int>> &aSchedule)
+{
+    for (int i = 0; i < aSchedule.size(); i++)
+    {
+        printf("%-4d", i + 1);
+        for (int x : aSchedule[i])
+        {
+            printf("%-4d", x);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+int main()
+{
+    vector<vector<int>> aSchedule = GetSchedule(4);
+    PrintSchedule(aSchedule);
+    return 0;
+}
